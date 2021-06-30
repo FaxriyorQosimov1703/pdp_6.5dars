3a5ae3db71577af93643608a59b2f206682d2a2a
Faxriyor <faxriyor@gmail.com>
2021-06-30 17:08:58 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import My from './myComponent'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    state = {
+        active: true
+    }
+
+    onClicked = () => {
+            this.setState({
+                active: true
+            })
+    }
+
+    onsClicked = () => {
+            this.setState({
+                active: false
+            })
+    }
+
+    render() {
+        const {active} = this.state
+        return (
+            <div className="container">
+                <div className="border mt-5">
+                    <div className="row">
+                        <div className="col-4">
+                            <button className="border p-3 m-3" onClick={this.onClicked}>show</button>
+                            <button className="border p-3 m-3" onClick={this.onsClicked}>hide</button>
+
+                        </div>
+                        <div className="col-8">
+                            <div className="border p-5 m-4">
+
+                                {
+                                    active?<My  />: ''
+                                }
+                                
+                                
+                                
+                                <hr/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
-
-export default App;
